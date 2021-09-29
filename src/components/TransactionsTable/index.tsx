a8bf3c8ceaaa7e88fcef243a5a54e2039164a7d5
GustavoGJesus <icarogabriel01@protonmail.com>
2021-09-28 23:36:37 -0300

    Listing transaction and seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from "./styles";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
+interface Transaction{
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable(){
-    useEffect(() => {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions));
     },[]);
 
     return(
@@ -21,18 +32,16 @@ export function TransactionsTable(){
                 </thead>
                 
                 <tbody>
-                    <tr>
-                        <td>Website Development</td>
-                        <td className="deposit">$600.00</td>
-                        <td>Developer</td>
-                        <td>20/05/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Pizza</td>
-                        <td className="withdraw">- $10.00</td>
-                        <td>Food</td>
-                        <td>18/05/2021</td>
-                    </tr>
+                    {transactions.map(transaction => {
+                        return(
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>{transaction.amount}</td>
+                                <td>{transaction.category}</td>
+                                <td>{transaction.createdAt}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody> 
             </table>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs'
+import { ActiveModelSerializer, createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
   models: {
     transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id: 1,
+          title: 'Freelancer',
+          type: 'deposit',
+          category: 'Developer',
+          amount: 7000,
+          createdAt: new Date('2021-09-28 23:00'),
+        },
+        {
+          id: 2,
+          title: 'Shop',
+          type: 'withdraw',
+          category: 'Buy',
+          amount: 1000,
+          createdAt: new Date('2021-06-24 17:00'),
+        }
+      ]
+    })
   },
   
   routes(){
